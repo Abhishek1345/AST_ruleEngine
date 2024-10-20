4955afb3a8b6287a6f81b76a275e1038dfba7bd8
AYUSHSAHU2004 <141218241+AYUSHSAHU2004@users.noreply.github.com>
2024-10-20 16:12:26 +0530

    added mongo

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,28 @@
 const express=require('express');
 const cors=require("cors");
 const app=express();
+const mongoose = require('mongoose');
+
+
 app.use(cors());
+
+require('dotenv').config();
+
+const mongoURI = process.env.MONGO_URI;
+
+
+mongoose.connect(mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => {
+    console.log('Connected to MongoDB Atlas');
+})
+.catch((err) => {
+    console.error('Error connecting to MongoDB Atlas:', err);
+});
+
+
 app.listen(8080,(error)=>{
         if(error){
             console.error("error in setting up the server:",error);
